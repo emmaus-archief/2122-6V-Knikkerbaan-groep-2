810554cefcc0e06e8762193fc5f6e07160d09160
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2021-11-30 10:13:21 +0100

    Update _main.ino

--- arduino/knikkerbaan/_main.ino
+++ arduino/knikkerbaan/_main.ino
@@ -1,6 +1,6 @@
 #include <Arduino_JSON.h>
 
-KnikkerPoort poortBoven = KnikkerPoort (BOVEN_POORT_PIN, 0, 90);
+KnikkerPoort poortBoven;
 WiFiCommunicator wifi = WiFiCommunicator(WIFI_NETWERK, WIFI_WACHTWOORD, SERVER_DOMEINNAAM);
 Teller tellerA = Teller(TELLER_A_PIN);
 
@@ -9,6 +9,7 @@ unsigned long tijdVoorContactMetServer = 0;
 
 void setup() {
   Serial.begin(9600);
+  poortBoven = KnikkerPoort (BOVEN_POORT_PIN, 0, 90);
 
   wifi.begin();
 
@@ -39,7 +40,7 @@ void loop() {
 
     wifi.stuurVerzoek("/api/set/sensordata", data.c_str());
 
-    tijdVoorContactMetServer = millis() + serverContactInterval * 1000;
+    tijdVoorContactMetServer = millis() + (unsigned long)serverContactInterval * 1000;
 
 
     // en zet nu het poortje weer open:
