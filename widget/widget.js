0a7a68bd4c641b7bcad52ff40f4639d5b1caa6b6
heleke <helke.nadiene@gmail.com>
2022-02-17 17:28:16 +0000

    widget verbeterd (knikkerbaan design af)

--- widget/widget.js
+++ widget/widget.js
@@ -56,11 +56,58 @@ function draw() {
   line(50, 20, 135, 60);
   line(250, 20, 165, 60);
   
-  // streep naar trechter 
-  line(75, 175, 145, 100); 
+  // streep naar trechter --- wss weg 
+  line(85, 175, 145, 100); 
+  
+  /*
+  // strepen naar trechter 
+  line(45, 140, 145, 100);
+  line(10, 140, 50, 180); 
+  */
 
-  // streep naar rad
+  // streep naar rad ------- wss weg 
   line(200, 175, 145, 100);
+
+  /*
+  // strepen naar rad
+  line(235, 135, 145, 100);
+  line(275, 140, 240, 185);
+  */
+
+  // strepen bij rad
+  line(90, 385, 280, 300); // streep onder het rad
+  line(280, 300, 280, 200); // streep naast het rad
+
+  // strepen bij trechter 
+  line(25, 375, 125, 435); // streep direct onder
+  line(180, 445, 75, 500); 
+  line(15, 505, 130, 580); 
+
+  // scherm 
+  rect(165, 485, 120, 50);
+
+  // trechter 
+  line(25, 200, 25, 300); // wanden
+  line(100, 200, 100, 300); 
+  line(25, 300, 50, 325); // inlopende delen
+  line(100, 300, 75, 325); 
+  line(50, 325, 50, 350);
+  line(75, 325, 75, 350);
+
+  stroke(0, 0, 0);
+  strokeWeight(2);
+  line(15, 215, 115, 235); // stukjes erdoorheen 
+  line(15, 255, 115, 245);
+  line(15, 265, 115, 295);
+
+  // rad 
+  stroke(0,0,0); 
+  strokeWeight(8);
+  line(220, 250, 270, 240); // rechts
+  line(220, 250, 220, 200); // top
+  line(220, 250, 170, 240); // links
+  line(220, 250, 255, 300); // rechts onder
+  line(220, 250, 185, 300); // links onder
 
   // arduino elementen
   teller.show();
